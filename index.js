7cda5088fdac012ef4c9625192eac81c30e41c29
M7Pedroso <mb.pedroso7@gmail.com>
2023-10-08 13:35:27 -0300

    Meu Segundo Commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-let vitorias = ""
+
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+ 
+    if (vitorias < 10) {
+      nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+    } else {
+      nivel = "Imortal";
+    }
+    
+  
+    console.log("O Herói tem um saldo de " + saldoVitorias + " e está no nível de " + nivel);
+  }
+  
+  // Exemplo de uso da função:
+  calcularNivel(92, 0); // Isso imprimirá: "O Herói tem um saldo de 30 e está no nível de Prata"
